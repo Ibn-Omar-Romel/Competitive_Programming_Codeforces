953113ae668735aa14bb4de8cf35f81e1a8b295f
Ibn Omar Romel <ibnomarromel@gmail.com>
2025-07-27 00:43:17 +0600

    Update

---
+++ Educational Round 181/B_Left_and_Down.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long int
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define vsort sort( vec.begin(), vec.end())
+#define asort sort( veca, veca+size )
+#define ok cout << "ok" << endl
+#define nl cout << endl
+
+// loops
+#define fr(a, b) for(int i = a; i < b; i++) 
+
+
+void solve() {
+
+    ll a, b, k;
+    cin >> a >> b >> k;
+
+    if (a <= k and b <= k) {
+        cout << 1 << endl;
+    }
+    else {
+        
+    }
+}
+
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while( t-- ) {
+        solve();
+    }
+    return 0;
+}
--- Round 966/B_Seating_in_a_Bus.cpp
+++ Round 966/B_Seating_in_a_Bus.cpp
@@ -14,6 +14,83 @@ using namespace std;
 #define fr(a, b) for(int i = a; i < b; i++) 
 
 
+// void solve() {
+
+//     int n ;
+//     cin >> n;
+
+//     vector<int>vec(n);
+//     for ( int i = 0 ; i < n ; i++ ) {
+//         cin >> vec[i];
+//     }
+
+//     vector<int>freq(n+1, 0);
+//     freq[0] = 1;
+
+//     int occ = 0;
+//     int flag = 0;
+//     for ( int i = 0 ; i < n; i++ ) {
+
+//         if ( occ == 0 ) {
+//             freq[vec[i]]++;
+//             occ = 1;
+//         }
+//         else if ( vec[i] == 1 ) {
+//             if ( freq[2] == 1 ){
+//                 freq[vec[i]]++;
+//             }
+//             else{
+//                 flag = 1;
+//                 break;
+//             }
+//         }
+//         else if ( vec[i] == n ){
+//             if ( freq[n-1] == 1 ){
+//                 freq[vec[i]]++;
+//             }
+//             else{
+//                 flag = 1;
+//                 break;
+//             }
+//         }
+//         else {
+//             if ( freq[vec[i]+1] == 1 or freq[vec[i]-1] == 1){
+//                 freq[vec[i]]++;
+//             }
+//             else{
+
+//                 flag = 1;
+//                 break;
+//             }
+//         }
+//     }
+    
+//     flag == 0 ? yes:no;
+// }
+
+
+void solve() {
+    
+    int n;
+    cin >> n;
+
+    unordered_set<int>st;
+    bool flag = true;
+
+    for (int i = 0; i < n; i++) {
+
+        int x; cin >> x;
+
+        if (!st.empty() and st.find(x-1) != st.end() and st.find(x+1) != st.end()) {
+            flag = false;
+        }
+        st.insert(x);
+    }
+
+    flag? yes:no;
+}
+
+
 int main() {
 
     ios_base::sync_with_stdio(false);
@@ -24,57 +101,7 @@ int main() {
     cin >> t;
 
     while( t-- ) {
-        
-        int n ;
-        cin >> n;
-
-        vector<int>vec(n);
-        for ( int i = 0 ; i < n ; i++ ) {
-            cin >> vec[i];
-        }
-    
-        vector<int>freq(n+1, 0);
-        freq[0] = 1;
-
-        int occ = 0;
-        int flag = 0;
-        for ( int i = 0 ; i < n; i++ ) {
-
-            if ( occ == 0 ) {
-                freq[vec[i]]++;
-                occ = 1;
-            }
-            else if ( vec[i] == 1 ) {
-                if ( freq[2] == 1 ){
-                    freq[vec[i]]++;
-                }
-                else{
-                    flag = 1;
-                    break;
-                }
-            }
-            else if ( vec[i] == n ){
-                if ( freq[n-1] == 1 ){
-                    freq[vec[i]]++;
-                }
-                else{
-                    flag = 1;
-                    break;
-                }
-            }
-            else {
-                if ( freq[vec[i]+1] == 1 or freq[vec[i]-1] == 1){
-                    freq[vec[i]]++;
-                }
-                else{
-
-                    flag = 1;
-                    break;
-                }
-            }
-        }
-        
-        flag == 0 ? yes:no;
+        solve();
     }
     return 0;
 }
